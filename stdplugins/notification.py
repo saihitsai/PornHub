5f6cb376585dac3a75d55bfbeb2dc6e9b6d0e67e
saihitsai <62397588+saihitsai@users.noreply.github.com>
2020-03-24 13:47:18 +0530

    Update notification.py

--- stdplugins/notification.py
+++ stdplugins/notification.py
@@ -21,7 +21,7 @@ PREV_REPLY_MESSAGE = {}
 BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
 TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
 UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = "[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](t.me/r4v4n4) \n\n Thank you for contacting me but i Am Offline Now, i will message once I get online....."
+UNIBORG_USER_BOT_NO_WARN = "──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀\n\n Thank you for contacting me but i Am Offline Now, i will message once I get online....."
 
 
 @borg.on(admin_cmd(pattern="nccreatedch"))
@@ -97,7 +97,7 @@ async def approve_p_m(event):
                     await PREV_REPLY_MESSAGE[chat.id].delete()
                     del PREV_REPLY_MESSAGE[chat.id]
                 pmpermit_sql.approve(chat.id, reason)
-                await event.edit("[──███▅▄▄▄▄▄▄▄▄▄\n─██▐████████████\n▐█▀████████████▌▌\n▐─▀▀▀▐█▌▀▀███▀█─▌\n▐▄───▄█───▄█▌▄█](t.me/r4v4n4) \n\n My Master Has Approved You To PM Me...")
+                await event.edit("──███▅▄▄▄▄▄▄▄▄▄\n─██▐████████████\n▐█▀████████████▌▌\n▐─▀▀▀▐█▌▀▀███▀█─▌\n▐▄───▄█───▄█▌▄█ \n\n My Master Has Approved You To PM Me...")
                 await asyncio.sleep(30)
                 await event.delete()
 
